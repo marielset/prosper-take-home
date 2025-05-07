7d13e0350def567da7477879121bb37bd63d1077
Mariel Setton <marielsetton@Mac.lan>
2025-05-06 22:13:31 -0400

    task 3 pre testing

--- src/services/findAppointments.ts
+++ src/services/findAppointments.ts
@@ -17,19 +17,21 @@ async function findAppointmentsForPatient(patient: Patient) {
       appointments: true,
     },
   });
-  const assessmentPairsByClinician = clinicians.map((c: Clinician) => ({
-    firstName: c.firstName,
-    lastName: c.lastName,
-    slots: mergeSlots(c.availableSlots, c),
-  }));
+  const assessmentPairsByClinician = await Promise.all(
+    clinicians.map(async (c: Clinician) => ({
+      firstName: c.firstName,
+      lastName: c.lastName,
+      slots: await mergeSlots(c.availableSlots, c),
+    }))
+  );
   return assessmentPairsByClinician;
 }
 
-function mergeSlots(slots: AvailableAppointmentSlot[], clinician: Clinician) {
-  const filteredSlots = filterUnusableDates(
-    slots,
-    clinician.clinicianType === ClinicianTypes[0] ? 60 : 90
-  );
+async function mergeSlots(
+  slots: AvailableAppointmentSlot[],
+  clinician: Clinician
+) {
+  const filteredSlots = await filterUnusableDates(slots, clinician);
   const assessmentPairs = filteredSlots.reduce(
     (pairAcc, slotA, i, arr) => {
       for (let j = i + 1; j < arr.length; j++) {
@@ -47,23 +49,102 @@ function mergeSlots(slots: AvailableAppointmentSlot[], clinician: Clinician) {
   return assessmentPairs;
 }
 
-function filterUnusableDates(
+async function filterUnusableDates(
   slots: AvailableAppointmentSlot[],
-  duration: number
+  clinician: Clinician
 ) {
   if (slots.length === 0) return slots;
+  const duration = clinician.clinicianType === ClinicianTypes[0] ? 60 : 90;
+  const weeklyAppointmentCounts = {};
+  const dailyAppointmentCounts = {};
+
   let current_comparison = slots[0];
   const newDateList = [current_comparison];
-  slots.forEach((slot) => {
+  for (const slot of slots) {
     if (
       slot.date.getTime() - current_comparison.date.getTime() >=
-      duration * 60000
+        duration * 60000 &&
+      (await canAddDailyAppointment(
+        clinician,
+        dailyAppointmentCounts,
+        slot.date
+      )) &&
+      (await canAddWeeklyAppointment(
+        clinician,
+        weeklyAppointmentCounts,
+        slot.date
+      ))
     ) {
       newDateList.push(slot);
       current_comparison = slot;
     }
-  });
+  }
   return newDateList;
+}
+
+async function canAddDailyAppointment(
+  clinician: Clinician,
+  dailyCounts: Record<string, number>,
+  appointmentDate: Date
+) {
+  const startOfTheDay = new Date(appointmentDate);
+  startOfTheDay.setUTCHours(0, 0, 0, 0);
+  if (!(startOfTheDay.getTime() in dailyCounts)) {
+    const count = await checkDailyAppointments(startOfTheDay, clinician.id);
+    dailyCounts[startOfTheDay.getTime()] = count;
+  }
+
+  return dailyCounts[startOfTheDay.getTime()] < clinician.maxDailyAppointments;
+}
+
+async function canAddWeeklyAppointment(
+  clinician: Clinician,
+  weeklyCounts: Record<string, number>,
+  appointmentDate: Date
+) {
+  const startOfTheWeek = getStartOfTheWeek(appointmentDate);
+  if (!(startOfTheWeek.getTime() in weeklyCounts)) {
+    const count = await checkWeeklyAppointments(startOfTheWeek, clinician.id);
+    weeklyCounts[startOfTheWeek.getTime()] = count;
+  }
+  return (
+    weeklyCounts[startOfTheWeek.getTime()] < clinician.maxWeeklyAppointments
+  );
+}
+
+async function checkWeeklyAppointments(
+  startOfTheWeek: Date,
+  clinicianId: string
+) {
+  const endOfTheWeek = new Date(startOfTheWeek);
+  endOfTheWeek.setDate(startOfTheWeek.getDate() + 7);
+  return await prisma.appointment.count({
+    where: {
+      clinicianId: clinicianId,
+      scheduledFor: { gte: startOfTheWeek, lt: endOfTheWeek },
+    },
+  });
+}
+
+function getStartOfTheWeek(appointmentDate: Date) {
+  const startOfTheWeek = new Date(appointmentDate);
+  startOfTheWeek.setDate(appointmentDate.getDate() - appointmentDate.getDay());
+  startOfTheWeek.setUTCHours(0, 0, 0, 0);
+  return startOfTheWeek;
+}
+
+async function checkDailyAppointments(
+  startOfTheDay: Date,
+  clinicianId: string
+) {
+  const endOfTheDay = new Date(startOfTheDay);
+  endOfTheDay.setUTCDate(endOfTheDay.getUTCDate() + 1);
+  return await prisma.appointment.count({
+    where: {
+      clinicianId: clinicianId,
+      scheduledFor: { gte: startOfTheDay, lt: endOfTheDay },
+    },
+  });
 }
 
 async function findPatient(id: string) {
